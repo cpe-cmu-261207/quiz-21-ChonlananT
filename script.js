3d776ee4387e1377f5145c91f4314013ef2e4771
Chonlanan Thongthai <chonlanan_t@cmu.ac.th>
2022-08-04 14:13:53 +0700

    finished

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+callApi();
+const tGender = document.getElementById("span-gender-icon");
+const tImage = document.getElementById("img-profile");
+const tName = document.getElementById("p-name");
+const tEmail = document.getElementById("p-email");
+const tAddress = document.getElementById("p-address");
+const btn = document.getElementById("btn-random");
+
+let gender;
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  if (resp.data.results[0].gender == "female") {
+    tGender.innerText = "👩";
+  } else tGender.innerText = "👨";
+
+  tImage.src = resp.data.results[0].picture.large;
+  tName.innerText =
+    resp.data.results[0].name.first + " " + resp.data.results[0].name.last;
+  tEmail.innerText = resp.data.results[0].email;
+
+  tAddress.innerText =
+    resp.data.results[0].location.city +
+    " " +
+    resp.data.results[0].location.state +
+    " " +
+    resp.data.results[0].location.country +
+    " " +
+    resp.data.results[0].location.postcode;
+
+  btn.onclick = () => {
+    callApi();
+  };
 }
